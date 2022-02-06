00ede25d01037e3b6364e4fd1939714ac2ed6946
salamanderman234 <tresnasaputra9@gmail.com>
2022-02-06 21:08:29 +0800

    Fitur Music Bot

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 import discord
 from discord.ext import commands
+import dotenv
+import os
 import music
 from asyncio import sleep
 # from asyncio import sleep
 
+
+token = os.getenv("BOT_TOKEN")
 cogs = [music]
 activity = discord.Game(name="Youtube")
 bot = commands.Bot(command_prefix='!',activity=activity, status=discord.Status.idle)
@@ -13,4 +17,4 @@ for i in cogs:
 
 
 #untuk running bot
-bot.run('OTM4MDAzNDM3NTI4OTU3MDE4.Yfj9xg.AoFMK2xoozHOHeax7dQSA37lmCY')
+bot.run(token)
